e5a6bd71b6bea9fdfdc884bd6b5f51b1dac34183
jamarob <janmartinrobert@gmail.com>
2022-04-11 12:09:02 +0200

    Initialize project using Create React App

---
+++ src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import {useState} from "react";
+
+function Counter () {
+    const [count,setCount] = useState(42)
+
+    const countUp = () => {
+        setCount(count + 1)
+    }
+
+    const countDown = () => {
+        setCount(count - 1)
+    }
+
+    const clear = () => {
+        setCount(0)
+    }
+
+    return <div>
+        <h1>Count {count}</h1>
+        <button onClick={countUp}>count up</button>
+        <button onClick={countDown}>count down</button>
+        <button onClick={clear}>Clear</button>
+    </div>
+}
+
+export default Counter
