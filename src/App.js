9be3fd53217b017a3d8b15fa819423273d2206f1
jamarob <janmartinrobert@gmail.com>
2022-04-11 14:26:54 +0200

    Add controlled form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,124 @@
-import "./App.css"
-import Counter from "./components/Counter";
+import './App.css'
+import { useState } from 'react'
 
-function App() {
-  return <Counter />;
+const initialFormData = {
+  name: 'Gwen Doe',
+  hours: 2,
+  fluffiness: 50,
+  color: 'red',
+  spits: true,
+  bites: false,
+  extraMean: true,
+  message: 'This is a very important message',
 }
 
-export default App;
+function App() {
+  const [formData, setFormData] = useState(initialFormData)
+
+  const handleChange = event => {
+    const value = event.target.value
+    const name = event.target.name
+    const newFormData = { ...formData, [name]: value }
+    setFormData(newFormData)
+    console.log(newFormData)
+  }
+
+  const handleCheckboxChange = event => {
+    const checked = event.target.checked
+    const name = event.target.name
+    const newFormData = { ...formData, [name]: checked }
+    setFormData(newFormData)
+    console.log(newFormData)
+  }
+
+  return (
+    <div>
+      <form>
+        <label>
+          Name{' '}
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Hours{' '}
+          <input
+            type="number"
+            name="hours"
+            value={formData.hours}
+            onChange={handleChange}
+            min="2"
+            max="12"
+          />
+        </label>
+        <br />
+        <label>
+          Fluffiness{' '}
+          <input
+            type="range"
+            name="fluffiness"
+            value={formData.fluffiness}
+            onChange={handleChange}
+            min="1"
+            max="100"
+          />
+        </label>
+        <br />
+        <label>
+          Color{' '}
+          <select value={formData.color} onChange={handleChange} name="color">
+            <option value="white">White</option>
+            <option value="brown">Brown</option>
+            <option value="red">Rot</option>
+            <option value="black">Schwarz</option>
+          </select>
+        </label>
+        <fieldset>
+          <legend>Extras</legend>
+          <label>
+            spits{' '}
+            <input
+              type="checkbox"
+              name="spits"
+              checked={formData.spits}
+              onChange={handleCheckboxChange}
+            />
+          </label>
+          <label>
+            bites{' '}
+            <input
+              type="checkbox"
+              name="bites"
+              checked={formData.bites}
+              onChange={handleCheckboxChange}
+            />
+          </label>
+          <label>
+            extra eman{' '}
+            <input
+              type="checkbox"
+              name="extraMean"
+              checked={formData.extraMean}
+              onChange={handleCheckboxChange}
+            />
+          </label>
+        </fieldset>
+        <br />
+        <label>
+          Message
+          <textarea
+            value={formData.message}
+            onChange={handleChange}
+            name="message"
+          />
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default App
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,26 +1,28 @@
-import {useState} from "react";
+import { useState } from 'react'
 
-function Counter () {
-    const [count,setCount] = useState(42)
+function Counter() {
+  const [count, setCount] = useState(42)
 
-    const countUp = () => {
-        setCount(count + 1)
-    }
+  const countUp = () => {
+    setCount(count + 1)
+  }
 
-    const countDown = () => {
-        setCount(count - 1)
-    }
+  const countDown = () => {
+    setCount(count - 1)
+  }
 
-    const clear = () => {
-        setCount(0)
-    }
+  const clear = () => {
+    setCount(0)
+  }
 
-    return <div>
-        <h1>Count {count}</h1>
-        <button onClick={countUp}>count up</button>
-        <button onClick={countDown}>count down</button>
-        <button onClick={clear}>Clear</button>
+  return (
+    <div>
+      <h1>Count {count}</h1>
+      <button onClick={countUp}>count up</button>
+      <button onClick={countDown}>count down</button>
+      <button onClick={clear}>Clear</button>
     </div>
+  )
 }
 
 export default Counter
